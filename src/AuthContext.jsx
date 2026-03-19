66dcbd651ffeab9418f960f43ea2b8dbc9b937c8
Evan Miller-Jones <evan.millerjones@gmail.com>
2026-03-19 14:48:56 -0500

    block 406 - hw - attempt 1

--- src/AuthContext.jsx
+++ src/AuthContext.jsx
@@ -9,10 +9,52 @@ export function AuthProvider({ children }) {
   const [location, setLocation] = useState("GATE");
 
   // TODO: signup
+  const signup = async(formData)=>{
+    const username = formData.get("name")
+
+    const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup', 
+              { 
+                method: "POST", 
+                headers: { 
+                  "Content-Type": "application/json" 
+                }, 
+                body: JSON.stringify({ 
+                  username: username
+                }) 
+              })   
+    const data = await response.json();
+    console.log(data.token);
+    setToken(data.token);
+    setLocation("TABLET");
+  }
+
 
   // TODO: authenticate
+  const authenticate = async()=>{
+    if (token){
+      const response = await fetch('https://fsa-jwt-practice.herokuapp.com/authenticate', 
+                { 
+                  method: "GET", 
+                  headers: { 
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}` 
+                  }
+                })
+      if (!response.ok){
+        throw new Error("Bad token");
+      }
+      setLocation("TUNNEL");
+    }
+    else{
+      throw new Error("No token");
+    }
+  }
 
-  const value = { location };
+
+
+
+
+  const value = { location, signup, authenticate};
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
